8731521cfa7795b40d95fed632bffa5760ec493e
dmsurabaya <itdmsurabaya@outlook.com>
2022-08-01 15:30:19 +0700

    m

---
+++ admin/lib/dbcon.php
@@ -0,0 +1,29 @@
+<?php
+//core
+
+	$user = "root";
+	$pass = "";
+	$host = "localhost";
+	$db = "thesis";
+	$koneksi = mysqli_connect($host,$user,$pass,$db);
+	// mysqli_select_db($koneksi,$db);
+
+
+function host(){
+	$h = "http://".$_SERVER['HTTP_HOST']."/thesis/";
+	return $h;
+}
+
+function hRoot(){
+	$url = $_SERVER['DOCUMENT_ROOT']."/thesis/";
+	return $url;
+}
+
+//parse string
+function gstr(){
+    $qstr = $_SERVER['QUERY_STRING'];
+    parse_str($qstr,$dstr);
+    return $dstr;
+}
+
+?>
