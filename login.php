48202de6aa34d1d11de20f0040ebda678fd5ffec
dmsurabaya <itdmsurabaya@outlook.com>
2022-08-03 13:56:42 +0700

    test

--- login.php
+++ login.php
@@ -13,24 +13,24 @@
 			$num_row = mysqli_num_rows($result);
 			
 		/*...................................................Technical Staff ..............................................*/
-		// $query_client = mysqli_query($koneksi, "SELECT * FROM client WHERE username='$username' AND password='$password'")or die(mysqli_error());
-		// $num_row_client = mysqli_num_rows($query_client);
-		// $row_client = mysqli_fetch_array($query_client);
+		$query_client = mysqli_query($koneksi, "SELECT * FROM client WHERE username='$username' AND password='$password'")or die(mysqli_error());
+		$num_row_client = mysqli_num_rows($query_client);
+		$row_client = mysqli_fetch_array($query_client);
 		
-		// if( $num_row > 0 ) { 
-		// $_SESSION['id']=$row['admin_id'];
-		// echo 'true_admin';
+		if( $num_row > 0 ) { 
+		$_SESSION['id']=$row['admin_id'];
+		echo 'true_admin';
 		
-		// mysqli_query("insert into user_log (username,login_date,admin_id)values('$username',NOW(),".$row['admin_id'].")")or die(mysqli_error());
+		mysqli_query("insert into user_log (username,login_date,admin_id)values('$username',NOW(),".$row['admin_id'].")")or die(mysqli_error());
 		
-		// }else if ($num_row_client > 0){
-		// $_SESSION['client']=$row_client['client_id'];
-		// echo 'true';
+		}else if ($num_row_client > 0){
+		$_SESSION['client']=$row_client['client_id'];
+		echo 'true';
 		
 		mysqli_query($koneksi,"insert into user_log (username,login_date,client_id)values('$username',NOW(),".$row_client['client_id'].")")or die(mysqli_error());
 		
-		//  }else{ 
-		// 		echo 'false';
-		// }	
+		 }else{ 
+				echo 'false';
+		}	
 				
 		?>
